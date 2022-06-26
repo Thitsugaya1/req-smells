bce3aac1a9c792ef485d1438e98cce4dc1a8f22f
Thitsugaya1 <nikho38@gmail.com>
2022-06-26 17:40:47 -0400

    v1.2

--- memoriaWebPage/settings.py
+++ memoriaWebPage/settings.py
@@ -126,12 +126,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+   ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_DIRS = [
-    STATIC_ROOT
-]
